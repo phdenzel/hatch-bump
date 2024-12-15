30dd48cc282a65949d90ed917654337db8031440
phdenzel <phdenzel@gmail.com>
2024-12-15 16:47:21 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+bumpType=$1
+
+runner () {
+    echo "🟡 starting $@"
+    $@ && echo "🟢 $@ passed" || (echo "🔴 $@ failed" && exit 1)
+}
+
+if [[ -n "$GITHUB_WORKSPACE" ]]; then
+    git config --global --add safe.directory $GITHUB_WORKSPACE
+fi
+
+git config --global user.name ${GITHUB_ACTOR}
+git config --global user.email "hatchbump[bot]@users.noreply.github.com"
+
+if [ -n "$(git status --porcelain)" ]; then
+    # Working directory clean
+    # Uncommitted changes
+    echo "🔴 There are uncommitted changes, exiting"
+    exit 1
+fi
+
+
+VERSION=`hatch version`
+
+case $bumpType in
+
+    'release')
+        hatch version release;;
+    'major')
+        hatch version major;;
+    'minor')
+        hatch version minor;;
+    'micro' | 'patch' | 'fix')
+        hatch version micro;;
+    'alpha')
+        hatch version alpha;;
+    'beta')
+        hatch version beta;;
+    'c' | 'rc' | 'pre' | 'preview')
+        hatch version pre;;
+    'r' | 'rev' | 'post')
+        hatch version rev;;
+    'dev')
+        hatch version dev;;
+    *)
+        echo "🔵 Skipped Version Bump";;
+esac
+
+NEW_VERSION=`hatch version`
+
+if [ "$VERSION" != "$NEW_VERSION" ]; then
+    echo "🟢 Success: bump version: $VERSION → $NEW_VERSION"
+
+    git add .
+    git commit -m "Bump version: $VERSION → $NEW_VERSION"
+    git remote -v
+
+    echo "🟢 Success version push"
+
+    git push
+fi
+
+exit 0
